7e9cde37a613c8247329ab8821d6ac8a92fe1b12
dbakemp <delroystar@hotmail.com>
2015-11-18 14:21:16 +0100

    DIt!!!!

--- GOINSP/GOINSP/Models/Inspection.cs
+++ GOINSP/GOINSP/Models/Inspection.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GOINSP.Models
 {
-    class Context : DbContext
+    public class Inspection
     {
-        public DbSet<Inspection> Inspection { get; set; }
-
-        public Context()
-            : base()
+        public Inspection()
         {
 
         }
+
+        [Key]
+        public int InspectionID { get; set; }
     }
 }
