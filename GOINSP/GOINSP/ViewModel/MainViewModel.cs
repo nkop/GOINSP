30ee15790dfd0756c57911fce80485966478b5bd
Delroy van der Kemp <dbakemp@avans.nl>
2015-11-26 19:24:07 +0100

    menu

--- GOINSP/GOINSP/ViewModel/MainViewModel.cs
+++ GOINSP/GOINSP/ViewModel/MainViewModel.cs
@@ -9,19 +9,25 @@ namespace GOINSP.ViewModel
     {
         private UserControl _userControlWindow;
         public ICommand UserControlCommand { get; set; }
+        public ICommand OpenDataImportCommand { get; set; }
 
         public MainViewModel()
         {
             _userControlWindow = new UserControl();
 
             UserControlCommand = new RelayCommand(ShowUserControl);
+            OpenDataImportCommand = new RelayCommand(ShowOpenDataImport);
         }
 
         private void ShowUserControl()
         {
             _userControlWindow.Show();
         }
-        
 
+        private void ShowOpenDataImport()
+        {
+            OpenDataImport _openDataImportWindow = new OpenDataImport();
+            _openDataImportWindow.Show();
+        }
     }
 }
