03e401ca13b1c0997ac3f7decb3cb5f5dabcfac1
Delroy van der Kemp <dbakemp@avans.nl>
2015-12-17 17:52:47 +0100

    gmapstest

--- GOINSP/GOINSP/OpenDataImport.xaml.cs
+++ GOINSP/GOINSP/OpenDataImport.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,8 @@ namespace GOINSP
         public OpenDataImport()
         {
             InitializeComponent();
+            map1.MapProvider = GMapProviders.GoogleMap;
+            map1.Position = new PointLatLng(51.6790508905935, 5.0618044538692);
         }
     }
 }
