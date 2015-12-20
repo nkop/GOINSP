71a33973af0a97795925083c1a57e4be16f2ccdf
Delroy van der Kemp <dbakemp@avans.nl>
2015-12-20 23:12:23 +0100

    radio question

--- GOINSP/GOINSP/ViewModel/QuestionTemplateVM.cs
+++ GOINSP/GOINSP/ViewModel/QuestionTemplateVM.cs
@@ -30,25 +30,23 @@ namespace GOINSP.ViewModel
         {
             QuestionnaireCollection = new ObservableCollection<Question>();
             SimpleBoolQuestion simpleBool = new SimpleBoolQuestion() { ListNumber = 1, Visible = Visibility.Visible };
-            SimpleTextQuestion simpleTextConditionBound = new SimpleTextQuestion() { ListNumber = 2, Visible = Visibility.Hidden, VisibleCondition = true };
-            SimpleTextQuestion simpleTextConditionBound2 = new SimpleTextQuestion() { ListNumber = 3, Visible = Visibility.Hidden, VisibleCondition = true };
-            SimpleTextQuestion simpleTextConditionBound3 = new SimpleTextQuestion() { ListNumber = 4, Visible = Visibility.Hidden, VisibleCondition = true };
-            SimpleTextQuestion simpleTextConditionBound4 = new SimpleTextQuestion() { ListNumber = 5, Visible = Visibility.Hidden, VisibleCondition = true };
-            SimpleTextQuestion simpleTextConditionBound5 = new SimpleTextQuestion() { ListNumber = 6, Visible = Visibility.Hidden, VisibleCondition = true };
+            SimpleTextQuestion simpleTextConditionBound = new SimpleTextQuestion() { ListNumber = 2, Visible = Visibility.Collapsed, VisibleCondition = true };
+            RadioQuestion radioQuestion = new RadioQuestion() { ListNumber = 3, Question = "Reden van capaciteitvermindering:", Visible = Visibility.Visible, AlternativeAnswerVisibility = Visibility.Visible };
+
+            radioQuestion.Answers = new List<RadioAnswer>();
+            radioQuestion.Answers.Add(new RadioAnswer() { Text = "bouwwerkzaamheden", GroupName = "group1", Checked = false });
+            radioQuestion.Answers.Add(new RadioAnswer() { Text = "schoonmaakwerkzaamheden", GroupName = "group1", Checked = false });
+            radioQuestion.Answers.Add(new RadioAnswer() { Text = "onderhoudswerkzaamheden", GroupName = "group1", Checked = false });
+            radioQuestion.Answers.Add(new RadioAnswer() { Text = "heggen snoeien / tuin onderhoud", GroupName = "group1", Checked = false });
+            radioQuestion.Answers.Add(new RadioAnswer() { Text = "event (vb concert)", GroupName = "group1", Checked = false });
+            radioQuestion.Answers.Add(new RadioAnswer() { Text = "verhuizing", GroupName = "group1", Checked = false });
 
             simpleBool.ConditionBoundQuestions = new List<Question>();
             simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound);
-            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound2);
-            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound3);
-            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound4);
-            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound5);
 
             questionnaireCollection.Add(simpleBool);
             questionnaireCollection.Add(simpleTextConditionBound);
-            questionnaireCollection.Add(simpleTextConditionBound2);
-            questionnaireCollection.Add(simpleTextConditionBound3);
-            questionnaireCollection.Add(simpleTextConditionBound4);
-            questionnaireCollection.Add(simpleTextConditionBound5);
+            questionnaireCollection.Add(radioQuestion);
         }
     }
 
@@ -117,9 +115,85 @@ namespace GOINSP.ViewModel
                 if (question.VisibleCondition == answer)
                     question.Visible = Visibility.Visible;
                 else
-                    question.Visible = Visibility.Hidden;
+                    question.Visible = Visibility.Collapsed;
             }
         }
+    }
+
+    public class RadioQuestion : Question
+    {
+        private string question;
+        public string Question
+        {
+            get
+            {
+                return question;
+            }
+            set
+            {
+                question = value;
+            }
+        }
+
+        private List<RadioAnswer> answers;
+        public List<RadioAnswer> Answers
+        {
+            get
+            {
+                return answers;
+            }
+            set
+            {
+                answers = value;
+            }
+        }
+
+        private string selectedAnswer;
+        public string SelectedAnswer
+        {
+            get
+            {
+                return selectedAnswer;
+            }
+            set
+            {
+                selectedAnswer = value;
+            }
+        }
+
+        private string alternativeAnswer;
+        public string AlternativeAnswer
+        {
+            get
+            {
+                return alternativeAnswer;
+            }
+            set
+            {
+                alternativeAnswer = value;
+            }
+        }
+
+        private Visibility alternativeAnswerVisibility;
+        public Visibility AlternativeAnswerVisibility
+        {
+            get
+            {
+                return alternativeAnswerVisibility;
+            }
+            set
+            {
+                alternativeAnswerVisibility = value;
+                RaisePropertyChanged("AlternativeAnswerVisibility");
+            }
+        }
+    }
+
+    public class RadioAnswer
+    {
+        public string Text { get; set; }
+        public bool Checked { get; set; }
+        public string GroupName { get; set; }
     }
 
     public class Question : ViewModelBase
