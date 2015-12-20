b36d5756bb4a7874f8b72e78e7b3469bacfd35dd
Delroy van der Kemp <dbakemp@avans.nl>
2015-12-20 19:53:44 +0100

    condition binding

--- GOINSP/GOINSP/ViewModel/QuestionTemplateVM.cs
+++ GOINSP/GOINSP/ViewModel/QuestionTemplateVM.cs
@@ -1,10 +1,12 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace GOINSP.ViewModel
@@ -27,12 +29,26 @@ namespace GOINSP.ViewModel
         public QuestionTemplateVM()
         {
             QuestionnaireCollection = new ObservableCollection<Question>();
-            QuestionnaireCollection.Add(new SimpleTextQuestion() { ListNumber = 1 });
-            QuestionnaireCollection.Add(new SimpleBoolQuestion() { ListNumber = 2 });
-            QuestionnaireCollection.Add(new SimpleBoolQuestion() { ListNumber = 3 });
-            QuestionnaireCollection.Add(new SimpleTextQuestion() { ListNumber = 4 });
-            QuestionnaireCollection.Add(new SimpleBoolQuestion() { ListNumber = 5 });
-            QuestionnaireCollection.Add(new SimpleTextQuestion() { ListNumber = 6 });
+            SimpleBoolQuestion simpleBool = new SimpleBoolQuestion() { ListNumber = 1, Visible = Visibility.Visible };
+            SimpleTextQuestion simpleTextConditionBound = new SimpleTextQuestion() { ListNumber = 2, Visible = Visibility.Hidden, VisibleCondition = true };
+            SimpleTextQuestion simpleTextConditionBound2 = new SimpleTextQuestion() { ListNumber = 3, Visible = Visibility.Hidden, VisibleCondition = true };
+            SimpleTextQuestion simpleTextConditionBound3 = new SimpleTextQuestion() { ListNumber = 4, Visible = Visibility.Hidden, VisibleCondition = true };
+            SimpleTextQuestion simpleTextConditionBound4 = new SimpleTextQuestion() { ListNumber = 5, Visible = Visibility.Hidden, VisibleCondition = true };
+            SimpleTextQuestion simpleTextConditionBound5 = new SimpleTextQuestion() { ListNumber = 6, Visible = Visibility.Hidden, VisibleCondition = true };
+
+            simpleBool.ConditionBoundQuestions = new List<Question>();
+            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound);
+            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound2);
+            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound3);
+            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound4);
+            simpleBool.ConditionBoundQuestions.Add(simpleTextConditionBound5);
+
+            questionnaireCollection.Add(simpleBool);
+            questionnaireCollection.Add(simpleTextConditionBound);
+            questionnaireCollection.Add(simpleTextConditionBound2);
+            questionnaireCollection.Add(simpleTextConditionBound3);
+            questionnaireCollection.Add(simpleTextConditionBound4);
+            questionnaireCollection.Add(simpleTextConditionBound5);
         }
     }
 
@@ -90,12 +106,40 @@ namespace GOINSP.ViewModel
             set
             {
                 answer = value;
+                CheckConditionBoundQuestions();
+            }
+        }
+
+        public void CheckConditionBoundQuestions()
+        {
+            foreach(Question question in ConditionBoundQuestions)
+            {
+                if (question.VisibleCondition == answer)
+                    question.Visible = Visibility.Visible;
+                else
+                    question.Visible = Visibility.Hidden;
             }
         }
     }
 
-    public class Question
+    public class Question : ViewModelBase
     {
         public int ListNumber { get; set; }
+
+        public Visibility visible;
+        public Visibility Visible {
+            get
+            {
+                return visible;
+            }
+            set
+            {
+                visible = value;
+                RaisePropertyChanged("Visible");
+            } 
+        }
+
+        public List<Question> ConditionBoundQuestions { get; set; }
+        public bool VisibleCondition { get; set; }
     }
 }
