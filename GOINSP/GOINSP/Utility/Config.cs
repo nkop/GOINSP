2216ed5b10d0f6da31f21095a8297af80b541ea5
Tim <timvbeers@icloud.com>
2016-01-14 09:46:09 +0100

    IK PUSH NAAR DE MASTER NANANANANA

---
+++ GOINSP/GOINSP/Utility/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOINSP.Utility
+{
+    public static class Config
+    {
+       public static Guid GebruikerID;
+       public static Models.Account.Rights Rechten;
+    }
+}
