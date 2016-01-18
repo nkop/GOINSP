8f7c9a42945fa0c60cf4ce8515f923a61e777367
Tim <timvbeers@icloud.com>
2016-01-18 14:17:08 +0100

    ANTI NIELS COMMIT #2
    pannekoek

--- GOINSP/GOINSP/ViewModel/AccountManagementVM.cs
+++ GOINSP/GOINSP/ViewModel/AccountManagementVM.cs
@@ -19,6 +19,8 @@ namespace GOINSP.ViewModel
         public ICommand LoginCommand { get; set; }
         public ICommand VergetenCommand { get; set; }
         public ICommand CreateAccountCommand { get; set; }
+
+        private AddUserWindow window;
         public ICommand ShowAddUserCommand { get; set; }
         public ICommand DeleteUserCommand { get; set; }
         public AccountVM SelectedAccount { get; set; }
@@ -106,7 +108,7 @@ namespace GOINSP.ViewModel
         {
             SelectedAccount = null;
             SelectedAccount = new AccountVM();
-            AddUserWindow window = new AddUserWindow();
+            window = new AddUserWindow();
             window.Show();
         }
 
@@ -126,6 +128,7 @@ namespace GOINSP.ViewModel
                     Email = "";
                     Config.Context.Account.Add(NewAccount);
                     Config.Context.SaveChanges();
+                    window.Close();
                     LoadUsers();
                 }
                 else
